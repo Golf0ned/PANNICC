5104475d23dc8469f61f3932f1603d2ba0a07e0c
Benjamin Ye <benjaminye.email@gmail.com>
2025-06-24 02:37:57 -0400

    expand base subset grammar -- function/instructions wip

--- src/frontend/parser.cpp
+++ src/frontend/parser.cpp
@@ -8,9 +8,78 @@ namespace pegtl = tao::pegtl;
 using namespace pegtl;
 
 namespace frontend {
-    struct grammar
-        : pegtl::star<pegtl::any> {};
 
+    //
+    // Grammar
+    //
+
+    // Tokens
+    struct keyword_return : TAO_PEGTL_STRING("return") {};
+
+    struct keyword_int64 : TAO_PEGTL_STRING("int64") {};
+
+    struct left_paren : pegtl::one<'('> {};
+    struct right_paren : pegtl::one<')'> {};
+    struct left_brace : pegtl::one<'{'> {};
+    struct right_brace : pegtl::one<'}'> {};
+    struct comma : pegtl::one<','> {};
+    struct semicolon : pegtl::one<';'> {};
+    struct plus : pegtl::one<'+'> {};
+    struct minus : pegtl::one<'-'> {};
+    struct asterisk : pegtl::one<'*'> {};
+    struct ampersand : pegtl::one<'&'> {};
+    
+    // Ignorables
+    struct comment
+        : pegtl::disable<TAO_PEGTL_STRING( "//" ),
+                         pegtl::until<pegtl::eolf>> {};
+
+    struct whitespace
+        : pegtl::star<pegtl::sor<pegtl::one<' '>,
+                                 pegtl::one<'\t'>,
+                                 pegtl::eol>> {};
+
+    struct ignorable
+        : pegtl::star<pegtl::seq<whitespace,
+                                 comment>> {};
+
+    // Identifiers and literals
+    struct identifier
+        : pegtl::seq<pegtl::plus<pegtl::sor<pegtl::alpha,
+                                            pegtl::one<'_'>>>,
+                     pegtl::star<pegtl::sor<pegtl::alnum,
+                                            pegtl::one<'_'>>>> {};
+
+    // Program structure
+    struct function
+        : pegtl::seq<keyword_int64,
+                     ignorable,
+                     identifier,
+                     ignorable,
+                     left_paren,
+                     ignorable,
+                     // TODO: parameter list
+                     right_paren,
+                     ignorable,
+                     left_brace,
+                     // TODO: instruction list
+                     right_brace,
+                     ignorable,
+                     semicolon> {};
+
+
+    struct entry_point
+        : pegtl::seq<ignorable,
+                     pegtl::star<function>,
+                     ignorable> {};
+
+    struct grammar
+        : pegtl::must<entry_point> {};
+
+
+    //
+    // Action
+    //
     template<typename Rule>
     struct action
         : pegtl::nothing<Rule> {};
