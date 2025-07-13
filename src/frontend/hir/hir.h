2712936dae97a81814e1001e839f0a77a8598710
Benjamin Ye <benjaminye.email@gmail.com>
2025-07-13 16:01:16 -0400

    move hir stuff, scaffold lowering

---
+++ src/frontend/hir/hir.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "frontend/hir/atom.h"
+#include "frontend/hir/instruction.h"
+
+namespace frontend::hir {
+    class Program {
+        public:
+            void addInstruction(Instruction* i);
+            std::string toString();
+            ~Program();
+        private:
+            std::vector<Instruction*> instructions;
+    };
+
+    class ToStringVisitor : public InstructionVisitor, public AtomVisitor {
+        public:
+            ToStringVisitor();
+            std::string getResult();
+
+            void visit(Atom* a) override;
+            void visit(AtomIdentifier* a) override;
+            void visit(AtomLiteral* a) override;
+
+            void visit(Instruction* i) override;
+            void visit(InstructionDeclaration* i) override;
+            void visit(InstructionAssignValue* i) override;
+            void visit(InstructionAssignBinaryOp* i) override;
+            void visit(InstructionReturn* i) override;
+
+        private:
+            std::string res;
+    };
+}
