0cf1ed8989dd8e5fe9be9233a0101a4bbd169198
Benjamin Ye <benjaminye.email@gmail.com>
2025-07-28 23:42:48 -0400

    duplicate atom for now, consider centralizing later

---
+++ src/frontend/hir/atom.cpp
@@ -0,0 +1,35 @@
+#include "frontend/hir/atom.h"
+
+
+namespace frontend::hir {
+    AtomIdentifier::AtomIdentifier(uint64_t value)
+        : value(value), identifier(true) {}
+
+    bool AtomIdentifier::isIdentifier() {
+        return identifier;
+    }
+
+    uint64_t AtomIdentifier::getValue() {
+        return value;
+    }
+
+    void AtomIdentifier::accept(AtomVisitor* visitor) {
+        visitor->visit(this);
+    }
+
+
+    AtomLiteral::AtomLiteral(uint64_t value)
+        : value(value), identifier(false) {}
+
+    bool AtomLiteral::isIdentifier() {
+        return identifier;
+    }
+
+    uint64_t AtomLiteral::getValue() {
+        return value;
+    }
+
+    void AtomLiteral::accept(AtomVisitor* visitor) {
+        visitor->visit(this);
+    }
+}
---
+++ src/frontend/hir/atom.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <cstdint>
+
+
+namespace frontend::hir {
+    class AtomVisitor;
+
+    class Atom {
+        public:
+            virtual bool isIdentifier() = 0;
+            virtual uint64_t getValue() = 0;
+            virtual void accept(AtomVisitor* visitor) = 0;
+            virtual ~Atom() = default;
+    };
+
+    class AtomIdentifier : public Atom {
+        public:
+            AtomIdentifier(uint64_t value);
+            bool isIdentifier() override;
+            uint64_t getValue() override;
+            void accept(AtomVisitor* visitor) override;
+        private:
+            bool identifier;
+            uint64_t value;
+    };
+
+    class AtomLiteral : public Atom {
+        public:
+            AtomLiteral(uint64_t value);
+            bool isIdentifier() override;
+            uint64_t getValue() override;
+            void accept(AtomVisitor* visitor) override;
+        private:
+            bool identifier;
+            uint64_t value;
+    };
+
+    class AtomVisitor {
+        public:
+            virtual void visit(Atom* a) = 0;
+            virtual void visit(AtomIdentifier* a) = 0;
+            virtual void visit(AtomLiteral* a) = 0;
+    };
+}
