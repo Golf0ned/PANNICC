ef932f9361b1581ab9d37c17902664b3e36f4803
Benjamin Ye <benjaminye.email@gmail.com>
2025-07-28 23:50:03 -0400

    hir function exists now

--- src/frontend/hir/hir.cpp
+++ src/frontend/hir/hir.cpp
@@ -2,7 +2,30 @@
 
 
 namespace frontend::hir {
-    // TODO: Function
+    Function::Function(Type type, AtomIdentifier* name, std::vector<Instruction*> body)
+        : type(type), name(name), body(body) {}
+
+    Type Function::getType() {
+        return type;
+    }
+
+    AtomIdentifier* Function::getName() {
+        return name;
+    }
+
+    std::vector<Instruction*> Function::getBody() {
+        return body;
+    }
+
+    std::string Function::toString(std::unordered_map<uint64_t, std::string>& symbol_table) {
+        std::string type_str = ::frontend::toString(type);
+        std::string name_str = symbol_table.at(name->getValue());
+
+        std::string res = type_str + " " + name_str + "()";
+        // TODO: add body string
+
+        return res;
+    }
 
 
     std::string Program::toString() {
--- src/frontend/hir/hir.h
+++ src/frontend/hir/hir.h
@@ -5,10 +5,23 @@
 #include <vector>
 #include <unordered_map>
 
+#include "frontend/hir/atom.h"
+#include "frontend/hir/instruction.h"
+
 
 namespace frontend::hir {
     class Function {
-        // TODO
+        public:
+            Function(Type type, AtomIdentifier* name, std::vector<Instruction*> body);
+            Type getType();
+            AtomIdentifier* getName();
+            std::vector<Instruction*> getBody();
+            std::string toString(std::unordered_map<uint64_t, std::string>& symbol_table);
+        private:
+            Type type;
+            AtomIdentifier* name;
+            // TODO: params
+            std::vector<Instruction*> body;
     };
 
     class Program {
@@ -19,6 +32,5 @@ namespace frontend::hir {
             std::unordered_map<uint64_t, std::string> symbol_table;
     };
 
-    // TODO: move to own file
-    class Instruction;
+    // TODO: tostring
 }
