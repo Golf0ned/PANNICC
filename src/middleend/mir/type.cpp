2c7ee99a96453dfdb311bb58bb5b599efb5156ee
Benjamin Ye <benjaminye.email@gmail.com>
2025-08-11 20:34:07 -0700

    mir types

---
+++ src/middleend/mir/type.cpp
@@ -0,0 +1,11 @@
+#include "middleend/mir/type.h"
+
+
+namespace middleend {
+    std::string toString(Type type) {
+        switch (type) {
+            case Type::I64: return "i64";
+            default:        return "UNKNOWN";
+        }
+    }
+}
---
+++ src/middleend/mir/type.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+
+namespace middleend {
+    enum class Type {
+        I64,
+    };
+
+    const std::unordered_map<std::string, Type> strToType = {
+        {"i64", Type::I64},
+    };
+
+    std::string toString(Type type);
+}
