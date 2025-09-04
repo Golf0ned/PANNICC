daab0b5268560ff5a544ec15babbdf66eda3c36b
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-03 19:17:09 -0500

    add ptr type to mir

--- src/middleend/mir/type.h
+++ src/middleend/mir/type.h
@@ -6,10 +6,12 @@
 namespace middleend {
     enum class Type {
         I64,
+        PTR,
     };
 
     const std::unordered_map<std::string, Type> strToType = {
         {"i64", Type::I64},
+        {"ptr", Type::PTR},
     };
 
     std::string toString(Type type);
