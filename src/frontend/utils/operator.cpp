d8c64e31ed9b028b84bcb66f5f1d3912b161b9ee
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-05 02:34:09 -0500

    fix operator slush...i messed up on stream

--- src/frontend/utils/operator.cpp
+++ src/frontend/utils/operator.cpp
@@ -16,5 +16,16 @@ namespace frontend {
         }
     }
 
-    std::string toString(BinaryOp op);
+    middleend::mir::BinaryOp toMIR(BinaryOp op) {
+        switch (op) {
+        case BinaryOp::ADD:
+            return middleend::mir::BinaryOp::ADD;
+        case BinaryOp::SUB:
+            return middleend::mir::BinaryOp::SUB;
+        case BinaryOp::MUL:
+            return middleend::mir::BinaryOp::MUL;
+        case BinaryOp::AND:
+            return middleend::mir::BinaryOp::AND;
+        }
+    }
 } // namespace frontend
--- src/middleend/mir/operator.cpp
+++ src/middleend/mir/operator.cpp
@@ -15,17 +15,4 @@ namespace middleend::mir {
             return "UNKNOWN";
         }
     }
-
-    middleend::mir::BinaryOp toMIR(BinaryOp op) {
-        switch (op) {
-        case BinaryOp::ADD:
-            return middleend::mir::BinaryOp::ADD;
-        case BinaryOp::SUB:
-            return middleend::mir::BinaryOp::SUB;
-        case BinaryOp::MUL:
-            return middleend::mir::BinaryOp::MUL;
-        case BinaryOp::AND:
-            return middleend::mir::BinaryOp::AND;
-        }
-    }
 } // namespace middleend::mir
