dc88b20bfb3c14f20dbbbb3b617c690240049efd
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-06 21:23:53 -0500

    implement to_string, something's crashing rn

--- src/middleend/mir/mir.h
+++ src/middleend/mir/mir.h
@@ -1,16 +1,18 @@
 #pragma once
 
+#include <cstdint>
 #include <vector>
 
+#include "middleend/mir/instruction.h"
 #include "middleend/mir/type.h"
 
 namespace middleend::mir {
-    class Instruction;
-    class Terminator;
 
     class BasicBlock {
     public:
         BasicBlock(std::vector<Instruction *> body, Terminator *terminator);
+        std::string toString(bool isEntry = false);
+        ~BasicBlock();
 
     private:
         std::vector<Instruction *> body;
@@ -21,6 +23,9 @@ namespace middleend::mir {
     public:
         Function(Type type, std::string name,
                  std::vector<BasicBlock> basic_blocks);
+        Type getType();
+        std::string getName();
+        std::string toString();
 
     private:
         Type type;
@@ -31,8 +36,34 @@ namespace middleend::mir {
     class Program {
     public:
         Program(std::vector<Function> functions);
+        std::string toString();
 
     private:
         std::vector<Function> functions;
+    };
+
+    class ToStringVisitor : public InstructionVisitor {
+    public:
+        ToStringVisitor() = default;
+        std::string getResult();
+
+        uint64_t resolveBasicBlock(BasicBlock *basic_block);
+        uint64_t resolveInstruction(Instruction *instruction);
+        uint64_t getNextIdentifier();
+        std::string valueToString(Value *v);
+
+        virtual void visit(InstructionBinaryOp *i);
+        virtual void visit(InstructionCall *i);
+        virtual void visit(InstructionAlloca *i);
+        virtual void visit(InstructionLoad *i);
+        virtual void visit(InstructionStore *i);
+
+        virtual void visit(TerminatorReturn *t);
+
+    private:
+        std::string result;
+        uint64_t counter;
+        std::unordered_map<BasicBlock *, uint64_t> basic_block_ids;
+        std::unordered_map<Instruction *, uint64_t> instruction_ids;
     };
 } // namespace middleend::mir
