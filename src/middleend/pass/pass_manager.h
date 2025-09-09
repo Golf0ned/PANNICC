ac5f49357b38758156bc770b9eaf79dd6298b237
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-08 21:20:20 -0500

    initial pass manager/pass work

---
+++ src/middleend/pass/pass_manager.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "middleend/mir/mir.h"
+#include "middleend/pass/pass.h"
+
+namespace middleend {
+    class PassManager {
+    public:
+        void addPass(std::unique_ptr<TransformPass> p);
+        void runPasses(mir::Program &mir);
+
+    private:
+        std::vector<std::unique_ptr<TransformPass>> passes;
+        std::vector<std::unique_ptr<AnalysisPass>> analyses;
+    };
+} // namespace middleend
