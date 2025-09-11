6b7f8a1f19c5f770009e82a89ce676006543f12e
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-10 21:14:01 -0500

    more mem2reg progress

--- src/middleend/pass/mem2reg.cpp
+++ src/middleend/pass/mem2reg.cpp
@@ -1,3 +1,8 @@
+#include <deque>
+#include <unordered_set>
+
+#include "middleend/mir/instruction.h"
+#include "middleend/mir/mir.h"
 #include "middleend/pass/mem2reg.h"
 
 namespace middleend {
@@ -10,8 +15,42 @@ namespace middleend {
             // allocas are the initial variables
 
             // just look at pg 29 of ssa book man
-            for (auto &bb : f.getBasicBlocks()) {
-                for (auto &i : bb.getInstructions()) {
+            std::unordered_set<mir::BasicBlock *> visited;
+            std::deque<mir::BasicBlock *> worklist;
+            std::unordered_map<mir::InstructionAlloca *, mir::Value *> curVal;
+            worklist.push_back(&f.getBasicBlocks()[0]);
+            while (!worklist.empty()) {
+                auto cur = worklist.back();
+                worklist.pop_back();
+
+                if (visited.contains(cur)) {
+                    // TODO: update phis
+                    continue;
+                }
+
+                for (auto &i : cur->getInstructions()) {
+                    // auto *alloca =
+                    //     dynamic_cast<mir::InstructionAlloca *>(i.get());
+                    // if (alloca)
+                    //     curVal[alloca] = nullptr;
+                    auto *load = dynamic_cast<mir::InstructionLoad *>(i.get());
+                    if (load) {
+                        // updateReachingDefinition(alloca, i)
+                        // replace use of alloca in next instruction with i;
+                    }
+                    auto *store =
+                        dynamic_cast<mir::InstructionStore *>(i.get());
+                    if (store) {
+                        // updateReachingDefinition(alloca, i)
+                        // create fresh variable v???? (done)
+                        // replace defn of alloca w/ v in i (done)
+                        // reaching definition[v] = reaching definition[alloca]
+                        // reaching definition[alloca] = v
+                    }
+                }
+
+                for (auto &next : cur->getDescendants()) {
+                    worklist.push_back(next);
                 }
             }
         }
