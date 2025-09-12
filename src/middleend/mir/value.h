76cfaf4daf17ad49b36153fc314dfab578e36cea
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-11 20:30:31 -0500

    track def use chains in mir

--- src/middleend/mir/value.h
+++ src/middleend/mir/value.h
@@ -1,17 +1,23 @@
 #pragma once
 
+#include <vector>
+
 #include "middleend/mir/type.h"
 
 namespace middleend::mir {
+    class Instruction;
+
     // TODO: function params
     class Value {
     public:
         Value(Type type);
         Type getType();
+        std::vector<Instruction *> &getUses();
         virtual ~Value() = default;
 
     private:
         Type type;
+        std::vector<Instruction *> uses;
     };
 
     class Literal : public Value {
