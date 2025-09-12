cc4b4b13f2c1b4485da1543a02e23ee86fd39cf1
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-11 21:39:28 -0500

    finish mem2reg

--- src/middleend/pass/mem2reg.h
+++ src/middleend/pass/mem2reg.h
@@ -3,8 +3,25 @@
 #include "middleend/pass/pass.h"
 
 namespace middleend {
-    class Mem2Reg : TransformPass {
+    class Mem2Reg : public TransformPass {
     public:
         void run(mir::Program &p) override;
+    };
+
+    class ReplaceUseVisitor : public mir::InstructionVisitor {
+    public:
+        ReplaceUseVisitor(mir::Value *oldValue, mir::Value *newValue);
+
+        void visit(mir::InstructionBinaryOp *i);
+        void visit(mir::InstructionCall *i);
+        void visit(mir::InstructionAlloca *i);
+        void visit(mir::InstructionLoad *i);
+        void visit(mir::InstructionStore *i);
+
+        void visit(mir::TerminatorReturn *t);
+
+    private:
+        mir::Value *oldValue;
+        mir::Value *newValue;
     };
 } // namespace middleend
