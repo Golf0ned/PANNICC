f6385fdc033165377c2d4cb5c78b6bad72c197e7
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-17 20:40:02 -0500

    add i1 type

--- src/middleend/mir/type.cpp
+++ src/middleend/mir/type.cpp
@@ -5,6 +5,8 @@
 namespace middleend::mir {
     std::string toString(Type type) {
         switch (type) {
+        case Type::I1:
+            return "i1";
         case Type::I64:
             return "i64";
         case Type::PTR:
