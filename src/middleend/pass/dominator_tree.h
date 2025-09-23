253e7c28bf54ee21e9b5a809e62c311eb5cb9f1c
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-22 23:32:09 -0500

    implement dominator tree analysis pass

--- src/middleend/pass/dominator_tree.h
+++ src/middleend/pass/dominator_tree.h
@@ -6,10 +6,14 @@ namespace middleend {
     class DominatorTree : public AnalysisPass {
     public:
         void run(mir::Program &p) override;
+        bool dominates(mir::BasicBlock *bb1, mir::BasicBlock *bb2);
+        mir::BasicBlock *getImmediateDominator(mir::BasicBlock *bb);
+        const std::vector<mir::BasicBlock *> &getDominees(mir::BasicBlock *bb);
 
     private:
-        std::unordered_map<mir::BasicBlock *, mir::BasicBlock *> predecessors;
-        std::unordered_map<mir::BasicBlock *, std::vector<mir::BasicBlock>>
-            successors;
+        std::unordered_map<mir::BasicBlock *, mir::BasicBlock *>
+            immediate_dominators;
+        std::unordered_map<mir::BasicBlock *, std::vector<mir::BasicBlock *>>
+            immediate_dominated;
     };
 } // namespace middleend
