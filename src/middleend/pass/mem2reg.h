e57625c00e1ec15455df8af9097a0a5caa48b193
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-22 20:14:55 -0500

    register dominator tree pass in mem2reg

--- src/middleend/pass/mem2reg.h
+++ src/middleend/pass/mem2reg.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "middleend/pass/dominator_tree.h"
 #include "middleend/pass/pass.h"
 
 namespace middleend {
     class Mem2Reg : public TransformPass {
     public:
         void run(mir::Program &p) override;
+        void registerAnalyses(
+            std::vector<std::unique_ptr<AnalysisPass>> &analyses) override;
+
+    private:
+        DominatorTree *dt;
     };
 
     class ReplaceUseVisitor : public mir::InstructionVisitor {
