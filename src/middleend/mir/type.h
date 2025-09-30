56db2dae291ac3e21583c124109f11c6c6cb2295
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-29 20:10:52 -0500

    add more type sizes in middleend

--- src/middleend/mir/type.h
+++ src/middleend/mir/type.h
@@ -6,15 +6,21 @@
 namespace middleend::mir {
     enum class Type {
         I1,
+        I16,
+        I32,
         I64,
         PTR,
     };
 
+    // clang-format off
     const std::unordered_map<std::string, Type> strToType = {
         {"i1", Type::I1},
+        {"i16", Type::I16},
+        {"i32", Type::I32},
         {"i64", Type::I64},
         {"ptr", Type::PTR},
     };
+    // clang-format on
 
     std::string toString(Type type);
 } // namespace middleend::mir
