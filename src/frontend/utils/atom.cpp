ba5554a9c40c1c6cffa68aef97c7b3484484e697
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-29 20:11:05 -0500

    add function type + add type to atoms

--- src/frontend/utils/atom.cpp
+++ src/frontend/utils/atom.cpp
@@ -2,21 +2,27 @@
 #include "frontend/utils/symbol_table.h"
 
 namespace frontend {
-    AtomIdentifier::AtomIdentifier(uint64_t value) : value(value) {}
+    AtomIdentifier::AtomIdentifier(uint64_t value, Type type)
+        : value(value), type(type) {}
 
     bool AtomIdentifier::isIdentifier() { return true; }
 
     uint64_t AtomIdentifier::getValue() { return value; }
 
+    Type AtomIdentifier::getType() { return type; }
+
     std::string AtomIdentifier::toString(SymbolTable &symbol_table) {
         return symbol_table.getSymbol(value);
     }
 
-    AtomLiteral::AtomLiteral(uint64_t value) : value(value) {}
+    AtomLiteral::AtomLiteral(uint64_t value, Type type)
+        : value(value), type(type) {}
 
     bool AtomLiteral::isIdentifier() { return false; }
 
     uint64_t AtomLiteral::getValue() { return value; }
+
+    Type AtomLiteral::getType() { return type; }
 
     std::string AtomLiteral::toString(SymbolTable &symbol_table) {
         return std::to_string(value);
