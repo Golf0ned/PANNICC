210a2612b4d6d225b1800694f6cfd2e74b1389af
Benjamin Ye <benjaminye.email@gmail.com>
2025-09-30 21:42:25 -0500

    trim symbol table api

--- src/frontend/utils/symbol_table.cpp
+++ src/frontend/utils/symbol_table.cpp
@@ -1,53 +1,25 @@
 #include <cstdint>
-#include <stdexcept>
 #include <string>
 
 #include "frontend/utils/symbol_table.h"
 
 namespace frontend {
-    std::string toScopedSymbol(std::string symbol, uint64_t scope) {
-        return symbol + '_' + std::to_string(scope);
-    }
-
-    uint64_t SymbolTable::addSymbol(std::string symbol, Type type) {
-        if (symbol_to_id.contains(symbol)) {
-            uint64_t id = symbol_to_id.at(symbol);
-            if (id_to_type.at(id) != type)
-                throw std::invalid_argument(
-                    "Type exists already for symbol \"" + symbol + "\"");
-            return id;
+    uint64_t SymbolTable::addSymbol(std::string symbol) {
+        if (hasSymbol(symbol)) {
+            return symbol_to_id.at(symbol);
         }
 
         uint64_t id = symbol_to_id.size();
         symbol_to_id[symbol] = id;
         id_to_symbol[id] = symbol;
-        id_to_type[id] = type;
         return id;
-    }
-
-    uint64_t SymbolTable::getId(std::string symbol) {
-        return symbol_to_id.at(symbol);
     }
 
     std::string SymbolTable::getSymbol(uint64_t id) {
         return id_to_symbol.at(id);
     }
 
-    Type SymbolTable::getType(uint64_t id) { return id_to_type.at(id); }
-
     bool SymbolTable::hasSymbol(std::string symbol) {
         return symbol_to_id.contains(symbol);
-    }
-
-    std::string SymbolTable::toString() {
-        std::string res = "";
-
-        for (const auto [id, symbol] : id_to_symbol) {
-            auto type = id_to_type.at(id);
-            res += "  " + std::to_string(id) + ": (" +
-                   ::frontend::toString(type) + ") " + symbol + "\n";
-        }
-
-        return res;
     }
 } // namespace frontend
--- src/frontend/utils/symbol_table.h
+++ src/frontend/utils/symbol_table.h
@@ -4,23 +4,15 @@
 #include <string>
 #include <unordered_map>
 
-#include "frontend/utils/type.h"
-
 namespace frontend {
-    std::string toScopedSymbol(std::string symbol, uint64_t scope);
-
     class SymbolTable {
     public:
-        uint64_t addSymbol(std::string symbol, Type type);
-        uint64_t getId(std::string symbol);
+        uint64_t addSymbol(std::string symbol);
         std::string getSymbol(uint64_t id);
-        Type getType(uint64_t id);
         bool hasSymbol(std::string symbol);
-        std::string toString();
 
     private:
         std::unordered_map<uint64_t, std::string> id_to_symbol;
-        std::unordered_map<uint64_t, Type> id_to_type;
         std::unordered_map<std::string, uint64_t> symbol_to_id;
     };
 } // namespace frontend
