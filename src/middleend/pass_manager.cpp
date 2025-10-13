b84096d4cbf3774535089da676c7aa5a8b106d0d
Benjamin Ye <benjaminye.email@gmail.com>
2025-10-13 18:28:47 -0500

    add addPass by string

--- src/middleend/pass_manager.cpp
+++ src/middleend/pass_manager.cpp
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include "middleend/pass_manager.h"
 #include "middleend/transform/inst_combine.h"
 #include "middleend/transform/mem2reg.h"
@@ -7,6 +9,17 @@ namespace middleend {
     void PassManager::addPass(std::unique_ptr<TransformPass> p) {
         p->registerAnalyses(analyses);
         passes.push_back(std::move(p));
+    }
+
+    void PassManager::addPass(const std::string &pass_name) {
+        if (pass_name == "mem2reg")
+            addPass(std::make_unique<middleend::Mem2Reg>());
+        else if (pass_name == "simplify_cfg")
+            addPass(std::make_unique<middleend::SimplifyCFG>());
+        else if (pass_name == "inst_combine")
+            addPass(std::make_unique<middleend::InstCombine>());
+        else
+            throw std::invalid_argument("invalid pass: \"" + pass_name + "\"");
     }
 
     void PassManager::runPasses(mir::Program &mir) {
