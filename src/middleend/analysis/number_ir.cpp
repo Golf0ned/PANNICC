d3c7719074d6cd21bbb188c4fb8576a590995ca0
Benjamin Ye <benjaminye.email@gmail.com>
2025-10-21 21:02:42 -0500

    update number_ir to run at function level

--- src/middleend/analysis/number_ir.cpp
+++ src/middleend/analysis/number_ir.cpp
@@ -2,23 +2,25 @@
 
 namespace middleend {
     void NumberIR::run(mir::Program &p) {
+        for (auto &f : p.getFunctions())
+            run(f.get());
+    }
+
+    void NumberIR::run(mir::Function *f) {
         uint64_t counter = 0;
+        auto entry = f->getEntryBlock();
+        for (auto &bb : f->getBasicBlocks()) {
+            basic_block_ids[bb.get()] = bb.get() == entry ? -1 : counter++;
 
-        for (auto &f : p.getFunctions()) {
-            auto entry = f->getEntryBlock();
-            for (auto &bb : f->getBasicBlocks()) {
-                basic_block_ids[bb.get()] = bb.get() == entry ? -1 : counter++;
-
-                for (auto &i : bb->getInstructions()) {
-                    auto v = dynamic_cast<mir::Value *>(i.get());
-                    if (v)
-                        instruction_ids[v] = counter++;
-                }
-
-                auto t = dynamic_cast<mir::Value *>(bb->getTerminator().get());
-                if (t)
-                    instruction_ids[t] = counter++;
+            for (auto &i : bb->getInstructions()) {
+                auto v = dynamic_cast<mir::Value *>(i.get());
+                if (v)
+                    value_ids[v] = counter++;
             }
+
+            auto t = dynamic_cast<mir::Value *>(bb->getTerminator().get());
+            if (t)
+                value_ids[t] = counter++;
         }
     }
 
@@ -27,8 +29,8 @@ namespace middleend {
     }
 
     uint64_t NumberIR::getNumber(mir::Value *i) {
-        if (instruction_ids.contains(i))
-            return instruction_ids.at(i);
+        if (value_ids.contains(i))
+            return value_ids.at(i);
         // If not found, -1
         // that's always going to be mapped to a basic block so who cares
         return -1;
