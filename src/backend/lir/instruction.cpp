81af0d2377514d5cde3ced0d49fdf1bf0596a31f
Benjamin Ye <benjaminye.email@gmail.com>
2025-10-24 21:35:24 -0500

    update backend data structures + implement everything for now

---
+++ src/backend/lir/instruction.cpp
@@ -0,0 +1,34 @@
+#include "backend/lir/instruction.h"
+
+namespace backend::lir {
+    InstructionMov::InstructionMov(DataSize size, Operand *dst, Operand *src)
+        : size(size), dst(dst), src(src) {}
+
+    void InstructionMov::accept(InstructionVisitor *visitor) {
+        visitor->visit(this);
+    }
+
+    InstructionPush::InstructionPush(DataSize size, Operand *src)
+        : size(size), src(src) {}
+
+    void InstructionPush::accept(InstructionVisitor *visitor) {
+        visitor->visit(this);
+    }
+
+    InstructionPop::InstructionPop(DataSize size, Operand *dst)
+        : size(size), dst(dst) {}
+
+    void InstructionPop::accept(InstructionVisitor *visitor) {
+        visitor->visit(this);
+    }
+
+    InstructionCall::InstructionCall(std::string label) : label(label) {}
+
+    void InstructionCall::accept(InstructionVisitor *visitor) {
+        visitor->visit(this);
+    }
+
+    void InstructionRet::accept(InstructionVisitor *visitor) {
+        visitor->visit(this);
+    }
+} // namespace backend::lir
