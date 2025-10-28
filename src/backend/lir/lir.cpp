1db03413311104987fa388d6948e153a0b02a59c
Benjamin Ye <benjaminye.email@gmail.com>
2025-10-27 20:55:30 -0500

    remove notion of block from lir

--- src/backend/lir/lir.cpp
+++ src/backend/lir/lir.cpp
@@ -1,16 +1,12 @@
 #include "backend/lir/lir.h"
 
 namespace backend::lir {
-    Block::Block(std::string label,
-                 std::list<std::unique_ptr<Instruction>> body)
-        : label(label), body(std::move(body)) {}
+    Program::Program(std::list<std::unique_ptr<Instruction>> instructions)
+        : instructions(std::move(instructions)) {}
 
-    std::string Block::getLabel() { return label; }
+    std::list<std::unique_ptr<Instruction>> &Program::getInstructions() {
+        return instructions;
+    }
 
-    std::list<std::unique_ptr<Instruction>> &Block::getBody() { return body; }
-
-    Program::Program(std::list<std::unique_ptr<Block>> blocks)
-        : blocks(std::move(blocks)) {}
-
-    std::list<std::unique_ptr<Block>> &Program::getBlocks() { return blocks; }
+    std::string Program::toString() { return ""; }
 } // namespace backend::lir
