cb666dae636e42c20566c4e73985c192d1925640
Benjamin Ye <benjaminye.email@gmail.com>
2025-10-28 21:34:33 -0500

    update traversal interface + scaffold some todos

--- src/middleend/utils/traversal.cpp
+++ src/middleend/utils/traversal.cpp
@@ -1,9 +1,15 @@
 #include "middleend/utils/traversal.h"
+#include "middleend/mir/mir.h"
 
 namespace middleend {
+    std::list<mir::BasicBlock *> traversePreorder(mir::Function *f) {
+        // TODO: implement
+        std::list<mir::BasicBlock *> traversal_order;
+        return traversal_order;
+    }
+
     void recursePostorder(mir::BasicBlock *bb, std::list<mir::BasicBlock *> &to,
-                          std::unordered_map<mir::BasicBlock *, uint64_t> &tn,
-                          uint64_t &counter) {
+                          TraversalOrderMap &tn, uint64_t &counter) {
         tn[bb]; // Prevent loops
         for (auto succ : bb->getSuccessors().getUniqueEdges())
             if (!tn.contains(succ))
@@ -12,10 +18,25 @@ namespace middleend {
         to.push_back(bb);
     }
 
-    void numberPostorder(
-        mir::BasicBlock *entry, std::list<mir::BasicBlock *> &traversal_order,
-        std::unordered_map<mir::BasicBlock *, uint64_t> &traversal_numbers) {
+    std::list<mir::BasicBlock *> numberPostorder(mir::Function *f,
+                                                 TraversalOrderMap &tom) {
+        std::list<mir::BasicBlock *> traversal_order;
+
+        // TODO: rewrite iterative
         uint64_t counter = 0;
-        recursePostorder(entry, traversal_order, traversal_numbers, counter);
+        recursePostorder(f->getEntryBlock(), traversal_order, tom, counter);
+
+        return traversal_order;
+    }
+
+    std::list<mir::BasicBlock *> traversePostorder(mir::Function *f) {
+        TraversalOrderMap tom;
+        return numberPostorder(f, tom);
+    }
+
+    std::list<mir::BasicBlock *> traverseLeastBranches(mir::Function *f) {
+        // TODO: implement
+        std::list<mir::BasicBlock *> traversal_order;
+        return traversal_order;
     }
 } // namespace middleend
--- src/middleend/utils/traversal.h
+++ src/middleend/utils/traversal.h
@@ -3,14 +3,15 @@
 #include "middleend/mir/mir.h"
 
 namespace middleend {
-    // TODO: preorder basic block iterator
-    // TODO: reverse postorder basic block iterator
+    using TraversalOrderMap = std::unordered_map<mir::BasicBlock *, uint64_t>;
 
-    void recursePostorder(mir::BasicBlock *bb, std::list<mir::BasicBlock *> &to,
-                          std::unordered_map<mir::BasicBlock *, uint64_t> &tn,
-                          uint64_t &counter);
+    // TODO: make iterators
 
-    void numberPostorder(
-        mir::BasicBlock *entry, std::list<mir::BasicBlock *> &traversal_order,
-        std::unordered_map<mir::BasicBlock *, uint64_t> &traversal_numbers);
+    std::list<mir::BasicBlock *> traversePreorder(mir::Function *f);
+
+    std::list<mir::BasicBlock *> numberPostorder(mir::Function *f,
+                                                 TraversalOrderMap &tom);
+    std::list<mir::BasicBlock *> traversePostorder(mir::Function *f);
+
+    std::list<mir::BasicBlock *> traverseLeastBranches(mir::Function *f);
 } // namespace middleend
