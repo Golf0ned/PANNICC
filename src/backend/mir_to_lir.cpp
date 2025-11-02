db3ea48d9e9037a7ea0579374fe07fe4e2d14747
Benjamin Ye <benjaminye.email@gmail.com>
2025-11-01 21:43:50 -0500

    add tree tiling to main mir_to_lir thing

--- src/backend/mir_to_lir.cpp
+++ src/backend/mir_to_lir.cpp
@@ -1,16 +1,17 @@
 #include "backend/mir_to_lir.h"
 #include "backend/lir_tree/gen_trees.h"
+#include "backend/lir_tree/tile_trees.h"
 #include "middleend/utils/traversal.h"
 
 namespace backend {
     lir::Program mirToLir(middleend::mir::Program &mir) {
         // Generate trees per instruction
         lir::OperandManager om;
-        lir_tree::TreeGenVisitor visitor(mir, om);
+        lir_tree::TreeGenVisitor tree_gen(mir, om);
         for (auto &f : mir.getFunctions()) {
             auto linearized = middleend::traverseTraces(f.get());
 
-            visitor.startFunction(f.get());
+            tree_gen.startFunction(f.get());
             auto &bbs = f->getBasicBlocks();
             for (auto iter = bbs.begin(); iter != bbs.end(); iter++) {
                 auto next = std::next(iter);
@@ -18,14 +19,14 @@ namespace backend {
                 auto bb = iter->get();
                 auto next_bb = next == bbs.end() ? nullptr : next->get();
 
-                visitor.startBasicBlock(bb, next_bb);
+                tree_gen.startBasicBlock(bb, next_bb);
                 for (auto &i : iter->get()->getInstructions())
-                    i->accept(&visitor);
+                    i->accept(&tree_gen);
             }
-            visitor.endFunction();
+            tree_gen.endFunction();
         }
 
-        auto program_trees = visitor.getResult();
+        auto program_trees = tree_gen.getResult();
 
         // Merge trees
         for (auto &function_trees : program_trees) {
@@ -33,16 +34,16 @@ namespace backend {
         }
 
         // Tile trees
-        std::list<std::unique_ptr<lir::Instruction>> instructions;
+        lir_tree::TreeTileVisitor tile_tree(om);
         for (auto &function_trees : program_trees) {
             for (auto &tree : function_trees) {
-                // TODO: tile trees
+                tree->accept(&tile_tree);
             }
         }
+        auto instructions = tile_tree.getResult();
 
         // Final cleanup
         lir::Program lir(std::move(instructions));
         return std::move(lir);
     }
-
 } // namespace backend
