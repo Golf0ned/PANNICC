45f57175b64b96e34a734ce52dedc078120f8ae7
Benjamin Ye <benjaminye.email@gmail.com>
2025-11-02 19:27:07 -0600

    wait we dont need a visitor!

---
+++ src/backend/lir_tree/merge_trees.cpp
@@ -0,0 +1,22 @@
+#include "backend/lir_tree/merge_trees.h"
+
+namespace backend::lir_tree {
+    std::list<Tree> TreeMerger::getResult() { return std::move(merged_trees); }
+
+    void TreeMerger::consumeTree(Tree &t) {
+        auto asm_tree = dynamic_cast<AsmNode *>(t.getRoot().get());
+        if (!asm_tree) {
+            current_trees.push_back(std::move(t));
+            return;
+        }
+
+        mergeTrees();
+        merged_trees.push_back(std::move(t));
+    }
+
+    void TreeMerger::mergeTrees() {
+        // TODO: do merging
+        merged_trees.splice(merged_trees.end(), current_trees);
+    }
+
+} // namespace backend::lir_tree
--- src/backend/lir_tree/merge_trees.h
+++ src/backend/lir_tree/merge_trees.h
@@ -3,22 +3,15 @@
 #include "backend/lir_tree/node.h"
 
 namespace backend::lir_tree {
-    class TreeMergeVisitor : public NodeVisitor {
+    class TreeMerger {
     public:
-        std::list<std::list<Tree>> getResult();
+        std::list<Tree> getResult();
 
-        virtual void visit(Node *n);
-        virtual void visit(RegisterNode *n);
-        virtual void visit(ImmediateNode *n);
-        virtual void visit(OpNode *n);
-        virtual void visit(AllocaNode *n);
-        virtual void visit(LoadNode *n);
-        virtual void visit(StoreNode *n);
-        virtual void visit(PhiNode *n);
-        virtual void visit(AsmNode *n);
+        void consumeTree(Tree &t);
+        void mergeTrees();
 
     private:
         std::list<Tree> current_trees;
-        std::list<std::list<Tree>> merged_trees;
+        std::list<Tree> merged_trees;
     };
 } // namespace backend::lir_tree
