73e6b17f6892dbe9561c785769bf709aaa8a943d
Benjamin Ye <benjaminye.email@gmail.com>
2025-11-06 20:39:48 -0600

    more tree merge shuffling

--- src/backend/lir_tree/merge_trees.cpp
+++ src/backend/lir_tree/merge_trees.cpp
@@ -3,17 +3,28 @@
 namespace backend::lir_tree {
     std::list<Tree> TreeMerger::getResult() { return std::move(merged_trees); }
 
-    void TreeMerger::consumeTree(Tree &t) {
-        current_trees.push_back(std::move(t));
+    void TreeMerger::mergeTrees(std::list<Tree> &trees) {
+        std::unordered_map<Node *, Tree &> last_def;
 
-        auto asm_tree = dynamic_cast<AsmNode *>(t.getRoot().get());
-        if (asm_tree)
-            mergeTrees();
-    }
+        // Given trees T1 and T2, and T1 uses V defined by T2
+        // Merge T2 into T1 if:
+        // - V is dead after T1 OR V is only used by T1
+        // - No instructions in [T2, T1) that depend on T2
+        //   - If V is variable: No uses in range
+        //   - If V is variable: No defs of variables used by T2 in range
+        //   - If V is mem access: No memory instruction in range
 
-    void TreeMerger::mergeTrees() {
-        // TODO: do merging
-        merged_trees.splice(merged_trees.end(), current_trees);
+        bool changed = true;
+        while (changed) {
+            changed = false;
+
+            auto iter = trees.end();
+            while (iter != trees.begin()) {
+                iter--;
+            }
+        }
+
+        merged_trees.splice(merged_trees.end(), trees);
     }
 
 } // namespace backend::lir_tree
