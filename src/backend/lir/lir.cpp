814af98ce45aaa49392d6cf216b1ce669a19f1ce
Benjamin Ye <benjaminye.email@gmail.com>
2025-11-18 20:05:00 -0600

    forgot to add whitespace

--- src/backend/lir/lir.cpp
+++ src/backend/lir/lir.cpp
@@ -18,60 +18,72 @@ namespace backend::lir {
     std::string ToStringVisitor::getResult() { return result; }
 
     void ToStringVisitor::visit(Instruction *i) {}
+
     void ToStringVisitor::visit(Label *l) {
         if (!result.empty())
             result += "\n\n";
         result += l->getName() + ':';
     }
+
     void ToStringVisitor::visit(InstructionMov *i) {
         if (!result.empty())
             result += '\n';
         result += "mov [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionPush *i) {
         if (!result.empty())
             result += '\n';
         result += "push [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionPop *i) {
         if (!result.empty())
             result += '\n';
         result += "pop [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionCmp *i) {
         if (!result.empty())
             result += '\n';
         result += "cmp [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionJmp *i) {
         if (!result.empty())
             result += '\n';
         result += "jmp [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionCJmp *i) {
         if (!result.empty())
             result += '\n';
         result += "cjmp [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionCall *i) {
         if (!result.empty())
             result += '\n';
         result += "call [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionRet *i) {
         if (!result.empty())
             result += '\n';
         result += "ret [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionPhi *i) {
         if (!result.empty())
             result += '\n';
         result += "phi [TODO]";
     }
+
     void ToStringVisitor::visit(InstructionVirtual *i) {
         i->getInstruction()->accept(this);
         result += "    # [virtual]";
     }
+
     void ToStringVisitor::visit(InstructionUnknown *i) {
         if (!result.empty())
             result += '\n';
