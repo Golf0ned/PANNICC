d42892373789f91f7fe2edb68449cdbe3a8f1e26
Benjamin Ye <benjaminye.email@gmail.com>
2025-11-18 20:04:19 -0600

    byebye tile visitor, hello tiler

--- src/backend/lir_tree/tile_trees.h
+++ src/backend/lir_tree/tile_trees.h
@@ -3,28 +3,16 @@
 #include "backend/lir_tree/node.h"
 
 namespace backend::lir_tree {
-    class TreeTileVisitor : public NodeVisitor {
+    class TreeTiler {
     public:
-        TreeTileVisitor(lir::OperandManager &om);
+        TreeTiler(lir::OperandManager &om);
 
         std::list<std::unique_ptr<lir::Instruction>> getResult();
 
-        virtual void visit(Node *n);
-        virtual void visit(RegisterNode *n);
-        virtual void visit(ImmediateNode *n);
-        virtual void visit(OpNode *n);
-        virtual void visit(AllocaNode *n);
-        virtual void visit(LoadNode *n);
-        virtual void visit(StoreNode *n);
-        virtual void visit(AsmNode *n);
+        void tile(Node *tree);
 
     private:
         lir::OperandManager &om;
         std::list<std::unique_ptr<lir::Instruction>> assembly;
-    };
-
-    class Tile {
-    public:
-    private:
     };
 } // namespace backend::lir_tree
