0918e8c07968cb4de7095b4ba6b10b90e2dc455e
Benjamin Ye <benjaminye.email@gmail.com>
2025-11-22 22:30:25 -0600

    rescaffold tree tiling + add tile class

--- src/backend/lir_tree/tile_trees.cpp
+++ src/backend/lir_tree/tile_trees.cpp
@@ -1,6 +1,10 @@
 #include "backend/lir_tree/tile_trees.h"
 
 namespace backend::lir_tree {
+    std::vector<Tile> tiles = {
+        StoreTile(),
+    };
+
     TreeTiler::TreeTiler(lir::OperandManager &om) : om(om) {}
 
     std::list<std::unique_ptr<lir::Instruction>> TreeTiler::getResult() {
@@ -14,18 +18,27 @@ namespace backend::lir_tree {
             return;
         }
 
-        auto reg_node = dynamic_cast<RegisterNode *>(tree);
-        if (reg_node) {
-            recursiveTile(reg_node);
+        std::list<std::unique_ptr<lir::Instruction>> instructions;
+
+        std::list<Node *> worklist = {tree};
+        while (!worklist.empty()) {
+            auto cur = worklist.back();
+            worklist.pop_back();
+
+            Tile *cur_tile = nullptr;
+            uint64_t min_cost = -1;
+            for (auto &tile : tiles) {
+                auto tile_cost = tile.getCost();
+                if (tile.matches(cur) && tile_cost < min_cost) {
+                    cur_tile = &tile;
+                    min_cost = tile_cost;
+                }
+            }
+
+            instructions.splice(instructions.begin(), cur_tile->toAsm(cur));
+            worklist.splice(worklist.end(), cur_tile->getRemaining(cur));
         }
 
-        auto store_node = dynamic_cast<StoreNode *>(tree);
-        if (store_node) {
-            recursiveTile(store_node);
-        }
+        assembly.splice(assembly.end(), instructions);
     }
-
-    void TreeTiler::recursiveTile(RegisterNode *tree) {}
-
-    void TreeTiler::recursiveTile(StoreNode *tree) {}
 } // namespace backend::lir_tree
--- src/backend/lir_tree/tile_trees.h
+++ src/backend/lir_tree/tile_trees.h
@@ -10,11 +10,23 @@ namespace backend::lir_tree {
         std::list<std::unique_ptr<lir::Instruction>> getResult();
 
         void tile(Node *tree);
-        void recursiveTile(RegisterNode *tree);
-        void recursiveTile(StoreNode *tree);
 
     private:
         lir::OperandManager &om;
         std::list<std::unique_ptr<lir::Instruction>> assembly;
     };
+
+    class Tile {
+    public:
+        uint64_t getCost();
+        virtual bool matches(Node *root) = 0;
+        virtual std::list<std::unique_ptr<lir::Instruction>>
+        toAsm(Node *root) = 0;
+        virtual std::list<Node *> getRemaining(Node *root) = 0;
+
+    private:
+        uint64_t cost;
+    };
+
+    class StoreTile : public Tile {};
 } // namespace backend::lir_tree
