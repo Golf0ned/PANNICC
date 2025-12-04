f778ea96ae5251b5c0ecf198c1b0084224fc7c1d
Benjamin Ye <benjaminye.email@gmail.com>
2025-12-03 21:37:57 -0600

    regenerate lea tests + add associativity tests

--- test/regression/input/lea.c
+++ test/regression/input/lea.c
@@ -14,6 +14,18 @@ int leaBISD(int base, int index) {
     int j = (scale * index) + displacement + base;
     int k = base + displacement + (scale * index);
     int l = displacement + base + (scale * index);
+    int m = base + ((index * scale) + displacement);
+    int n = displacement + ((index * scale) + base);
+    int o = (index * scale) + (base + displacement);
+    int p = (index * scale) + (displacement + base);
+    int q = base + (displacement + (index * scale));
+    int r = displacement + (base + (index * scale));
+    int s = base + ((scale * index) + displacement);
+    int t = displacement + ((scale * index) + base);
+    int u = (scale * index) + (base + displacement);
+    int v = (scale * index) + (displacement + base);
+    int w = base + (displacement + (scale * index));
+    int x = displacement + (base + (scale * index));
 
     return 0;
 }
