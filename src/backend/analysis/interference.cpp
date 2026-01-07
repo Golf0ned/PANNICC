a909f9960b1497b99126097a823f19f9c01bb81f
Benjamin Ye <benjaminye.email@gmail.com>
2026-01-06 20:49:38 -0600

    stub interference

---
+++ src/backend/analysis/interference.cpp
@@ -0,0 +1,7 @@
+#include "backend/analysis/interference.h"
+
+namespace backend {
+    Interference::Interference(Liveness &liveness) {
+        // TODO: generate interference graph from liveness provided
+    }
+} // namespace backend
