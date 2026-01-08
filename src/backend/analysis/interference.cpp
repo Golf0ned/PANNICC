d0b343567f1c1cc807c15872993e4108f1a8d6ee
Benjamin Ye <benjaminye.email@gmail.com>
2026-01-07 20:20:36 -0600

    mostly implement interference

--- src/backend/analysis/interference.cpp
+++ src/backend/analysis/interference.cpp
@@ -1,7 +1,46 @@
 #include "backend/analysis/interference.h"
 
 namespace backend {
-    Interference::Interference(Liveness &liveness) {
-        // TODO: generate interference graph from liveness provided
+    Interference::Interference(lir::Program &p) : program(p) {}
+
+    void Interference::addEdge(lir::Register *first, lir::Register *second) {}
+
+    void Interference::computeInterference(Liveness &liveness) {
+        auto om = program.getOm();
+
+        auto num_regs = om->getNumRegisters();
+        adj_matrix = std::vector<std::vector<bool>>(
+            num_regs, std::vector<bool>(num_regs, false));
+        reg_to_index.clear();
+
+        for (auto reg : lir::getAllRegisters()) {
+            auto new_reg = om->getRegister(reg);
+            for (auto &[prev_reg, _] : reg_to_index)
+                addEdge(prev_reg, new_reg);
+            reg_to_index[new_reg] = reg_to_index.size();
+        }
+
+        auto out = liveness.getOut();
+        // TODO: write gen getter
+        // auto gen = liveness.getGen();
+        std::vector<RegisterSet> gen(out.size());
+
+        for (size_t i = 0; i < out.size(); i++) {
+            auto &gen_i = gen[i], &out_i = out[i];
+            for (auto gen_reg : gen_i) {
+                // TODO: make this not ugly thanks
+                for (auto prev_gen_reg : gen_i) {
+                    if (gen_reg == prev_gen_reg)
+                        break;
+                    addEdge(gen_reg, prev_gen_reg);
+                }
+
+                for (auto out_reg : out_i) {
+                    if (gen_reg == out_reg)
+                        continue;
+                    addEdge(out_reg, gen_reg);
+                }
+            }
+        }
     }
 } // namespace backend
