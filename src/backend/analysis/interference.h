f4fcc175cebfc774f03af3ab79596984bba297bc
Benjamin Ye <benjaminye.email@gmail.com>
2026-01-07 20:20:36 -0600

    mostly implement interference

--- src/backend/analysis/interference.h
+++ src/backend/analysis/interference.h
@@ -5,9 +5,12 @@
 namespace backend {
     class Interference {
     public:
-        Interference(Liveness &liveness);
+        Interference(lir::Program &p);
+        void computeInterference(Liveness &liveness);
+        void addEdge(lir::Register *first, lir::Register *second);
 
     private:
+        lir::Program &program;
         std::unordered_map<lir::Register *, size_t> reg_to_index;
         std::vector<std::vector<bool>> adj_matrix;
     };
