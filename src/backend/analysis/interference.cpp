4467bfc8e6b10b0ea87ecf687a5268e5254d428d
Benjamin Ye <benjaminye.email@gmail.com>
2026-01-16 20:45:36 -0600

    rewrite interference to not interfere with my brain

--- src/backend/analysis/interference.cpp
+++ src/backend/analysis/interference.cpp
@@ -4,35 +4,29 @@
 #include "backend/lir/operand.h"
 
 namespace backend {
-    Interference::Interference(lir::Program &p) : om(p.getOm()) {}
+    InterferenceBuilder::InterferenceBuilder(lir::OperandManager *om)
+        : om(om) {}
 
-    void Interference::addRegister(lir::Register *reg) {
-        if (!reg_to_index.contains(reg))
-            reg_to_index[reg] = reg_to_index.size();
-    }
+    Interference InterferenceBuilder::getInterference() { return interference; }
 
-    void Interference::addEdge(lir::Register *first, lir::Register *second) {
-        auto first_idx = reg_to_index.at(first),
-             second_idx = reg_to_index.at(second);
+    bool InterferenceBuilder::sameReg(lir::Register *first,
+                                      lir::Register *second) {
+        auto first_num = first->getRegNum();
+        auto first_is_virtual = first_num == lir::RegisterNum::VIRTUAL;
 
-        adj_matrix[first_idx][second_idx] = true;
-        adj_matrix[second_idx][first_idx] = true;
-    }
+        auto second_num = second->getRegNum();
+        auto second_is_virtual = second_num == lir::RegisterNum::VIRTUAL;
 
-    bool Interference::sameReg(lir::Register *first, lir::Register *second) {
-        auto first_num = first->getRegNum(), second_num = second->getRegNum();
-
-        if (first_num == lir::RegisterNum::VIRTUAL &&
-            second_num == lir::RegisterNum::VIRTUAL) {
+        if (first_is_virtual && second_is_virtual) {
             auto first_virtual = static_cast<lir::VirtualRegister *>(first);
             auto second_virtual = static_cast<lir::VirtualRegister *>(second);
             return first_virtual->getName() == second_virtual->getName();
         }
 
-        if (first_num != lir::RegisterNum::VIRTUAL &&
-            second_num != lir::RegisterNum::VIRTUAL) {
-            auto first_64 = lir::toSized(first_num, lir::DataSize::QUADWORD);
-            auto second_64 = lir::toSized(second_num, lir::DataSize::QUADWORD);
+        if (!first_is_virtual && !second_is_virtual) {
+            auto size = lir::DataSize::QUADWORD;
+            auto first_64 = lir::toSized(first_num, size);
+            auto second_64 = lir::toSized(second_num, size);
             return first_64 == second_64;
         }
 
@@ -40,11 +34,10 @@ namespace backend {
     }
 
     std::vector<lir::Register *>
-    Interference::getSizedRegisters(lir::Register *reg) {
+    InterferenceBuilder::getSizedRegisters(lir::Register *reg) {
         auto reg_num = reg->getRegNum();
-        if (reg_num == lir::RegisterNum::VIRTUAL) {
+        if (reg_num == lir::RegisterNum::VIRTUAL)
             return {reg};
-        }
 
         return {
             om->getRegister(lir::toSized(reg_num, lir::DataSize::QUADWORD)),
@@ -52,92 +45,56 @@ namespace backend {
         };
     }
 
-    void Interference::addAllEdges(lir::Register *first,
-                                   lir::Register *second) {
+    void InterferenceBuilder::interfere(lir::Register *first,
+                                        lir::Register *second) {
+        if (sameReg(first, second))
+            return;
+
         auto first_regs = getSizedRegisters(first),
              second_regs = getSizedRegisters(second);
 
         for (auto first_reg : first_regs)
-            for (auto second_reg : second_regs)
-                addEdge(first_reg, second_reg);
-    }
-
-    void Interference::addPhysicalRegisters() {
-        auto &all_regs = lir::getAllRegisters();
-
-        for (auto reg : all_regs) {
-            auto new_reg_64 = om->getRegister(reg);
-            auto new_reg_32 =
-                om->getRegister(lir::toSized(reg, lir::DataSize::DOUBLEWORD));
-
-            addRegister(new_reg_64);
-            addRegister(new_reg_32);
-        }
-
-        for (auto reg1 : all_regs) {
-            for (auto reg2 : all_regs) {
-                if (reg1 == reg2)
-                    break;
-                addAllEdges(om->getRegister(reg1), om->getRegister(reg2));
+            for (auto second_reg : second_regs) {
+                interference[first].insert(second);
+                interference[second].insert(first);
             }
-        }
     }
 
-    void Interference::computeInterference(Liveness &liveness) {
-        auto num_regs = om->getNumRegisters();
-        adj_matrix = std::vector<std::vector<bool>>(
-            num_regs, std::vector<bool>(num_regs, false));
-        reg_to_index.clear();
+    Interference computeInterference(lir::Program &p, Liveness &l) {
+        auto om = p.getOm();
+        InterferenceBuilder ib(om);
 
-        addPhysicalRegisters();
+        auto &physical_regs = lir::getAllRegisters();
+        for (size_t r1 = 1; r1 < physical_regs.size(); r1++)
+            for (size_t r2 = 0; r2 < r1; r2++)
+                ib.interfere(om->getRegister(physical_regs[r1]),
+                             om->getRegister(physical_regs[r2]));
 
-        auto gen = liveness[0], out = liveness[3];
-        for (size_t f = 0; f < out.size(); f++) {
-            for (size_t i = 0; i < out[f].size(); i++) {
+        auto gen = l[0], out = l[3];
+        for (size_t f = 0; f < gen.size(); f++) {
+            for (size_t i = 0; i < gen[f].size(); i++) {
                 auto &gen_i = gen[f][i], &out_i = out[f][i];
-
-                for (auto gen_reg : gen_i)
-                    addRegister(gen_reg);
-                for (auto out_reg : out_i)
-                    addRegister(out_reg);
-
                 for (auto gen_reg : gen_i) {
-                    for (auto prev_gen_reg : gen_i) {
-                        if (sameReg(prev_gen_reg, gen_reg))
-                            continue;
-                        addAllEdges(prev_gen_reg, gen_reg);
-                    }
-
-                    for (auto out_reg : out_i) {
-                        if (sameReg(out_reg, gen_reg))
-                            continue;
-                        addAllEdges(out_reg, gen_reg);
-                    }
+                    for (auto prev_gen_reg : gen_i)
+                        ib.interfere(prev_gen_reg, gen_reg);
+                    for (auto out_reg : out_i)
+                        ib.interfere(out_reg, gen_reg);
                 }
             }
         }
+
+        return ib.getInterference();
     }
 
-    void Interference::printInterference() {
-        std::vector<lir::Register *> index_to_reg(reg_to_index.size());
-        for (auto &[reg, idx] : reg_to_index)
-            index_to_reg[idx] = reg;
-
-        size_t r = adj_matrix.size(), c = adj_matrix[0].size();
-        for (size_t i = 0; i < r; i++) {
-            std::cout << index_to_reg[i]->toString() << ": {";
-
-            bool first_element = true;
-            for (size_t j = 0; j < c; j++) {
-                if (adj_matrix[i][j]) {
-                    if (first_element)
-                        first_element = false;
-                    else
-                        std::cout << ", ";
-                    std::cout << index_to_reg[j]->toString();
-                }
+    void printInterference(Interference &i) {
+        // TODO: sort?
+        for (auto &[r1, r2_set] : i) {
+            std::cout << r1->toString() << ": {";
+            for (auto iter = r2_set.begin(); iter != r2_set.end(); iter++) {
+                if (iter != r2_set.begin())
+                    std::cout << ", ";
+                std::cout << (*iter)->toString();
             }
-
             std::cout << "}" << std::endl;
         }
     }
