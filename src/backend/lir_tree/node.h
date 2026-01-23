7086530ff04db64314fb69eca1be253799331226
Benjamin Ye <benjaminye.email@gmail.com>
2026-01-22 19:51:26 -0600

    remove deprecated TreeManager

--- src/backend/lir_tree/node.h
+++ src/backend/lir_tree/node.h
@@ -162,22 +162,4 @@ namespace backend::lir_tree {
         std::unordered_map<Node *, std::list<Node *>> tree_leaves;
         std::unordered_set<Node *> trees_with_memory_instruction;
     };
-
-    class TreeManager {
-    public:
-        void insertAsm(std::unique_ptr<Node> tree);
-        void insertTree(std::unique_ptr<Node> tree, std::list<Node *> leaves,
-                        bool has_memory_instruction);
-        std::unique_ptr<Node> pop();
-        std::list<Node *> &getLeaves(Node *tree);
-        bool hasMemInst(Node *tree);
-        void setMemInst(Node *tree);
-        bool empty();
-        std::string toString(lir::OperandManager *om);
-
-    private:
-        std::list<std::unique_ptr<Node>> trees;
-        std::unordered_map<Node *, std::list<Node *>> tree_leaves;
-        std::unordered_set<Node *> trees_with_memory_instruction;
-    };
 } // namespace backend::lir_tree
