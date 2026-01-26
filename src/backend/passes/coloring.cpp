5af7703f4e5eeb0047dc1dadf4672b77adaf6035
Benjamin Ye <benjaminye.email@gmail.com>
2026-01-25 22:32:34 -0600

    start degree < r for coloring

--- src/backend/passes/coloring.cpp
+++ src/backend/passes/coloring.cpp
@@ -3,7 +3,28 @@
 namespace backend {
     std::pair<bool, RegisterColoring> tryColor(lir::Program &lir,
                                                Interference &interference) {
+        int num_regs = 16;
+
         // TODO: color/"prune(?)" graph
+        std::vector<lir::Register *> colorable;
+        while (!interference.empty()) {
+            // TODO: avoid the infinite
+            for (auto &[reg, edges] : interference) {
+                if (edges.size() >= num_regs)
+                    continue;
+
+                if (reg->getRegNum() != lir::RegisterNum::VIRTUAL)
+                    continue;
+
+                colorable.push_back(reg);
+
+                for (auto neighbor : edges)
+                    interference[neighbor].erase(reg);
+                interference.erase(reg);
+                break;
+            }
+        }
+
         return {true, {}};
     }
 
