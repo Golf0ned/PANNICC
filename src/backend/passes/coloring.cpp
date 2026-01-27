73196159c0d91c696b6f340c23f6aa4bdfcebef9
Benjamin Ye <benjaminye.email@gmail.com>
2026-01-26 21:02:02 -0600

    finish degree < R

--- src/backend/passes/coloring.cpp
+++ src/backend/passes/coloring.cpp
@@ -5,15 +5,13 @@ namespace backend {
                                                Interference &interference) {
         int num_regs = 16;
 
-        // TODO: color/"prune(?)" graph
         std::vector<lir::Register *> colorable;
-        while (!interference.empty()) {
-            // TODO: avoid the infinite
+        bool changed = true;
+        while (changed) {
+            changed = false;
+
             for (auto &[reg, edges] : interference) {
                 if (edges.size() >= num_regs)
-                    continue;
-
-                if (reg->getRegNum() != lir::RegisterNum::VIRTUAL)
                     continue;
 
                 colorable.push_back(reg);
@@ -21,9 +19,23 @@ namespace backend {
                 for (auto neighbor : edges)
                     interference[neighbor].erase(reg);
                 interference.erase(reg);
+
+                changed = true;
                 break;
             }
+
+            if (!interference.empty()) {
+                // TODO: find node with min spill cost divided by degree, push
+                // onto stack
+
+                // For now, we just stick topmost node on the stack lol
+                colorable.push_back(interference.begin()->first);
+
+                changed = true;
+            }
         }
+
+        // TODO: assign registers
 
         return {true, {}};
     }
