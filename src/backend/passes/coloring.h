3393c9f2b65a7e80fe338208de14e8a9e126a808
Benjamin Ye <benjaminye.email@gmail.com>
2026-01-29 21:13:20 -0600

    commit colors

--- src/backend/passes/coloring.h
+++ src/backend/passes/coloring.h
@@ -37,6 +37,35 @@ namespace backend {
         RegisterColoring precolorings;
     };
 
+    class ColoringVisitor : public lir::InstructionVisitor {
+    public:
+        ColoringVisitor(const RegisterColoring &coloring,
+                        lir::OperandManager *om);
+
+        lir::Operand *tryColorOperand(lir::Operand *operand);
+
+        void visit(lir::Instruction *i);
+        void visit(lir::Label *l);
+        void visit(lir::InstructionMov *i);
+        void visit(lir::InstructionPush *i);
+        void visit(lir::InstructionPop *i);
+        void visit(lir::InstructionConvert *i);
+        void visit(lir::InstructionBinaryOp *i);
+        void visit(lir::InstructionSpecialOp *i);
+        void visit(lir::InstructionLea *i);
+        void visit(lir::InstructionCmp *i);
+        void visit(lir::InstructionJmp *i);
+        void visit(lir::InstructionCJmp *i);
+        void visit(lir::InstructionCall *i);
+        void visit(lir::InstructionRet *i);
+        void visit(lir::InstructionVirtualCall *i);
+        void visit(lir::InstructionUnknown *i);
+
+    private:
+        const RegisterColoring &coloring;
+        lir::OperandManager *om;
+    };
+
     RegisterColoring getPrecoloring(lir::Program &lir);
 
     std::pair<bool, RegisterColoring> tryColor(lir::Program &lir,
