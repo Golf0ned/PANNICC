f57a9a3374a5654c01a6803f638a66e696d24a15
Benjamin Ye <benjaminye.email@gmail.com>
2026-02-06 18:34:20 -0600

    add manyreg mem2reg only test + regenerate test output for now

--- src/backend/regalloc.cpp
+++ src/backend/regalloc.cpp
@@ -34,6 +34,8 @@ namespace backend {
                 }
 
                 spillLowestCost(f, spill_costs);
+                printColoring(coloring);
+                return;
             }
         }
     }
