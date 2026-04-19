5c2b9054f7cefd0afbb0b74268db83f7f709c35a
Benjamin Ye <benjaminye.email@gmail.com>
2026-04-18 19:04:28 -0500

    build out preprocessor actions more

--- src/frontend/preprocessor/preprocess.cpp
+++ src/frontend/preprocessor/preprocess.cpp
@@ -1,5 +1,132 @@
 #include "frontend/preprocessor/preprocess.h"
 
+#include <iostream>
+#include <unordered_map>
+
 namespace frontend {
     std::vector<std::string> parsed_clauses;
-}
+    std::unordered_map<std::string, std::string> define_map;
+
+    struct whitespace : pegtl::plus<pegtl::blank> {};
+
+    struct keyword_define : TAO_PEGTL_STRING("#define") {};
+    struct keyword_include : TAO_PEGTL_STRING("#include") {};
+
+    struct identifier : pegtl::identifier {};
+    struct rest_of_line : pegtl::until<pegtl::eolf> {};
+
+    //
+    // Body
+    //
+    struct body : pegtl::until<pegtl::eolf> {};
+
+    //
+    // Directives
+    //
+    // TODO: add support for args
+    struct directive_define : pegtl::seq<keyword_define, whitespace, identifier,
+                                         whitespace, rest_of_line> {};
+
+    struct braces_include
+        : pegtl::seq<pegtl::one<'<'>, pegtl::until<pegtl::one<'>'>>> {};
+    struct quotes_include
+        : pegtl::seq<pegtl::one<'\"'>, pegtl::until<pegtl::one<'\"'>>> {};
+    struct directive_include
+        : pegtl::seq<keyword_include, pegtl::opt<whitespace>,
+                     pegtl::sor<braces_include, quotes_include>,
+                     pegtl::until<pegtl::eolf>> {};
+
+    struct directive : pegtl::sor<pegtl_try<directive_define>,
+                                  pegtl_try<directive_include>> {};
+
+    //
+    // Rest of translation unit
+    //
+    struct line
+        : pegtl::seq<pegtl::opt<whitespace>, pegtl::sor<directive, body>> {};
+
+    struct grammar : pegtl::must<pegtl::until<pegtl::eof, line>> {};
+
+    //
+    // Actions
+    //
+    template <typename Rule> struct action {};
+
+    template <> struct action<body> {
+        template <typename Input>
+        static void apply(const Input &in, std::string res) {
+            std::string line = in.string();
+
+            // Replace "define" macros
+        }
+    };
+
+    template <> struct action<identifier> {
+        template <typename Input>
+        static void apply(const Input &in, std::string res) {
+            parsed_clauses.push_back(in.string());
+        }
+    };
+
+    template <> struct action<rest_of_line> {
+        template <typename Input>
+        static void apply(const Input &in, std::string res) {
+            std::string rest = in.string();
+            parsed_clauses.push_back(rest.erase(rest.size() - 1));
+        }
+    };
+
+    template <> struct action<directive_define> {
+        template <typename Input>
+        static void apply(const Input &in, std::string res) {
+            std::string replacement = parsed_clauses.back();
+            parsed_clauses.pop_back();
+
+            std::string to_replace = parsed_clauses.back();
+            parsed_clauses.pop_back();
+
+            define_map[to_replace] = replacement;
+        }
+    };
+
+    template <> struct action<braces_include> {
+        template <typename Input>
+        static void apply(const Input &in, std::string res) {
+            parsed_clauses.push_back(in.string());
+        }
+    };
+
+    template <> struct action<quotes_include> {
+        template <typename Input>
+        static void apply(const Input &in, std::string res) {
+            parsed_clauses.push_back(in.string());
+        }
+    };
+
+    template <> struct action<directive_include> {
+        template <typename Input>
+        static void apply(const Input &in, std::string res) {
+            std::string clause = parsed_clauses.back();
+            parsed_clauses.pop_back();
+
+            std::cout << "[TODO] include: " << clause << std::endl;
+
+            std::string path = clause.substr(1, clause.size() - 2);
+            if (clause[0] == '"') {
+                // TODO: handle quote include
+            }
+            // TODO: handle angle brace include + qutoe include fallback
+        }
+    };
+
+    std::string preprocess(const std::string &input_file) {
+        file_input in(input_file);
+
+        parsed_clauses.clear();
+
+        std::string preprocessed;
+        pegtl::parse<grammar, action>(in, preprocessed);
+
+        return preprocessed;
+    }
+} // namespace frontend
