ebbcb16344842322da94b6a9a73c6e3ed016be88
Benjamin Ye <benjaminye.email@gmail.com>
2026-04-20 19:53:53 -0500

    move parser to parser dir + split into string and file parse

--- src/frontend/parser/parser.cpp
+++ src/frontend/parser/parser.cpp
@@ -1,15 +1,29 @@
 #include <tao/pegtl/contrib/analyze.hpp>
 
-#include "frontend/parser.h"
 #include "frontend/parser/function.h"
+#include "frontend/parser/parser.h"
 
 namespace frontend {
     struct grammar
         : pegtl::must<pegtl::seq<ignorable, pegtl::star<function>, ignorable>> {
     };
 
-    ast::Program parse(const std::string &input_file) {
+    ast::Program parse_file(const std::string &input_file) {
         file_input in(input_file);
+
+        symbol_table = std::make_unique<SymbolTable>();
+        parsed_tokens.clear();
+        active_scopes.clear();
+
+        std::vector<ast::Function> parsed_functions;
+        pegtl::parse<grammar, action>(in, parsed_functions);
+
+        ast::Program ast(std::move(parsed_functions), std::move(symbol_table));
+        return ast;
+    }
+
+    ast::Program parse_string(const std::string &input) {
+        string_input in(input, "string_input");
 
         symbol_table = std::make_unique<SymbolTable>();
         parsed_tokens.clear();
---
+++ src/frontend/parser/parser.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+#include "frontend/ast/ast.h"
+
+namespace frontend {
+    ast::Program parse_file(const std::string &input_file);
+
+    ast::Program parse_string(const std::string &input);
+} // namespace frontend
