6d7d97c34b774ac6ded81e263edb40231d502896
Benjamin Ye <benjaminye.email@gmail.com>
2026-05-14 18:37:28 -0500

    fix main default logic

--- src/frontend/ast/to_mir.cpp
+++ src/frontend/ast/to_mir.cpp
@@ -188,7 +188,7 @@ void ToMIRVisitor::visit(ast::FunctionDefinition *f) {
     scope_binding_types.emplace_back();
     for (auto &i : f->getBody()->getInstructions())
         i->accept(this);
-    if (name == "main") {
+    if (name == "main" && (basic_blocks.size() == 0 || !instructions.empty())) {
         auto zero = getLiteral(0, mir::Type::I32);
         auto store = std::make_unique<mir::InstructionStore>(zero, ret_alloca);
         instructions.push_back(std::move(store));
