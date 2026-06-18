be1c0e061dffda469703ca73ed0758d0395ba4da
Benjamin Ye <benjaminye.email@gmail.com>
2026-06-18 18:21:10 -0500

    gvn hash + add number ir

--- src/middleend/transform/gvn.cpp
+++ src/middleend/transform/gvn.cpp
@@ -1,5 +1,7 @@
 #include "middleend/transform/gvn.h"
 
+#include <format>
+
 namespace middleend {
 GVN::GVN() : next_num(0) {}
 
@@ -22,6 +24,7 @@ void GVN::dvnt(mir::BasicBlock *bb) {
         auto phi = dynamic_cast<mir::InstructionPhi *>(i);
         if (phi) {
             // TODO: if meaningless or redundant, map to equivalent
+
             // TODO: else, insert new value number
         }
 
@@ -41,6 +44,41 @@ void GVN::dvnt(mir::BasicBlock *bb) {
         dvnt(c);
 }
 
+std::string GVN::gvnHash(mir::Value *v) {
+    // TODO: can we not use strings
+    // TODO: do we need to account for sizes
+
+    auto literal = dynamic_cast<mir::Literal *>(v);
+    if (literal)
+        return std::format("l{}", literal->getValue());
+
+    auto bin_op = dynamic_cast<mir::InstructionBinaryOp *>(v);
+    if (bin_op) {
+        auto op = bin_op->getOp();
+        auto left = bin_op->getLeft();
+        auto right = bin_op->getRight();
+
+        if (false
+            // clang-format off
+            || op == mir::BinaryOp::ADD
+            || op == mir::BinaryOp::MUL
+            || op == mir::BinaryOp::AND
+            || op == mir::BinaryOp::OR
+            || op == mir::BinaryOp::XOR
+            // clang-format on
+        ) {
+            auto tmp = left;
+            left = left < right ? left : right;
+            right = tmp < right ? right : tmp;
+        }
+
+        return std::format("b{} {} {}", mir::toString(op), gvnHash(left),
+                           gvnHash(right));
+    }
+
+    return std::format("v{}", nir->getNumber(v));
+}
+
 void GVN::registerAnalyses(
     std::vector<std::unique_ptr<AnalysisPass>> &analyses) {
     for (auto &pass : analyses) {
@@ -50,6 +88,12 @@ void GVN::registerAnalyses(
             required_analyses.push_back(dt);
             continue;
         }
+
+        auto nir = dynamic_cast<NumberIR *>(pass.get());
+        if (nir) {
+            this->nir = nir;
+            required_analyses.push_back(nir);
+        }
     }
 
     if (!this->dt) {
@@ -57,6 +101,13 @@ void GVN::registerAnalyses(
         this->dt = dt.get();
         required_analyses.push_back(dt.get());
         analyses.push_back(std::move(dt));
+    }
+
+    if (!this->nir) {
+        auto nir = std::make_unique<NumberIR>();
+        this->nir = nir.get();
+        required_analyses.push_back(nir.get());
+        analyses.push_back(std::move(nir));
     }
 }
 } // namespace middleend
--- src/middleend/transform/gvn.h
+++ src/middleend/transform/gvn.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "middleend/analysis/dominator_tree.h"
+#include "middleend/analysis/number_ir.h"
 #include "middleend/pass.h"
 
 namespace middleend {
@@ -14,14 +15,16 @@ public:
     void run(mir::Function *f);
 
     void dvnt(mir::BasicBlock *bb);
+    std::string gvnHash(mir::Value *v);
 
     void registerAnalyses(
         std::vector<std::unique_ptr<AnalysisPass>> &analyses) override;
 
 private:
     DominatorTree *dt;
+    NumberIR *nir;
     uint64_t next_num;
-    // TODO: figure out hash hell
+    std::unordered_map<std::string, uint64_t> value_to_num;
     std::unordered_map<uint64_t, mir::Value *> num_to_value;
 };
 
